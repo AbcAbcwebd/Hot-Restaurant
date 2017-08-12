d1a4d76a99e422f01bcc2c1edbb0157cba8c7bc4
Owen Essen <abc_abcwebd@yahoo.com>
2017-08-12 11:50:35 -0400

    Tinkering

--- server.js
+++ server.js
@@ -23,8 +23,6 @@ app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 })); 
-//app.use(express.json());       // to support JSON-encoded bodies
-//app.use(express.urlencoded()); // to support URL-encoded bodies
 
 
 // YOUR CODE GOES HERE
